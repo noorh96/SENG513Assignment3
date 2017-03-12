b706f0baebb7dbc72786db70f57a10affc2c474f
Noor Hammad <noor.hammad96@hotmail.ca>
2017-03-12 13:57:30 -0600

    70% complete, no cookies, chat history or bold messages.

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var io = require('socket.io').listen(server);
 
 users = [];
 connections = [];
+messages = [];
+
 
 server.listen(process.env.PORT||3000);
 
@@ -19,26 +21,41 @@ app.get('/',function(req,res){
 
 io.sockets.on('connection',function(socket){
     connections.push(socket);
-    users.push(createUniqueUsername());
+    users.push("User" + createUniqueUsername()); // Create a unique username for the user
+
     // New User
-    socket.username = users[users.length-1];
-    socket.emit('get user', socket.username);
-    updateUsernames(socket);
+    socket.username = users[users.length-1]; // Assign the username to socket
+    socket.emit('get user', socket.username); // send it to client so that they know who the current user is
+    updateUsernames(socket); // Send the client an updated list of usernames
     console.log('Connected: %s sockets connected', connections.length);
 
     //Disconnect
     socket.on('disconnect',function (data) {
-        users.splice(users.indexOf(connections.indexOf(socket.username),1));
-        connections.splice(connections.indexOf(socket),1);
+        users.splice(users.indexOf(connections.indexOf(socket.username),1)); // remove user from user array
+        connections.splice(connections.indexOf(socket),1); // remove connection
         console.log('Disconnect: %s sockets connected',connections.length);
     });
 
     //Send Message
     socket.on('send message', function(data){
-        io.sockets.emit('new message', {msg:data,time:getTime(),username:socket.username});
+        var color = 0;
+        var res = checkUsernameChange(data); // Check if /nick was entered
+        if(res) {
+            changeUsername(data, socket); // If /nick was entered, change the username in socket and user
+            socket.emit('get user', socket.username); // Let the client know that the username has changed
+            updateUsernames(socket); // emit the new list of usernames
+        }
+        res = checkColorChange(data); // Check if /nickcolor was entered
+        if(res){
+            color = setColor(data); // if /nickcolor was entered, return the desired color
+        }
+        messages.push({msg:data,time:getTime(),username:socket.username,color:color});
+            io.sockets.emit('new message', {msg:data,time:getTime(),username:socket.username,color:color});
     });
 });
 
+
+// Returns an array with time in minutes and hours
 function getTime(){
     var timeStamp = [];
     var date = new Date();
@@ -48,10 +65,13 @@ function getTime(){
 
 }
 
+// Emits to client the list of users
 function updateUsernames(socket){
     io.sockets.emit('get users',{users:users,currentUser:socket.username});
 }
 
+
+// Creates a unique username for the user (up to 10 users)
 function createUniqueUsername(){
     var id = Math.round(Math.random()*10);
     var iterations = 0;
@@ -70,4 +90,52 @@ function createUniqueUsername(){
 
     return id;
 
+}
+
+// Check if /nick was entered
+function checkUsernameChange(data){
+    data.toLowerCase();
+    var result = data.match(/\/nick/i);
+    if(result === null || result.length!=1)
+        return false;
+    else
+        return true;
+}
+// Check if the username exists
+function checkUsernameExist(data){
+    for(i = 0; i < users.length; i++){
+        if(data === users[i])
+            return false;
+    }
+    return true;
+}
+
+//Change the username in socket and user
+function changeUsername(data,socket){
+    var nickname = data.split(" ");
+    if (nickname.length > 1 && !(data.match(/\/nickcolor/i))){
+        if(checkUsernameExist(nickname[1])) {
+            users[users.indexOf(socket.username)] = nickname[1];
+            socket.username = nickname[1];
+        }
+    }
+}
+
+// Check if /nickcolor was entered
+function checkColorChange(data){
+    data.toLowerCase()
+    var result = data.match(/\/nickcolor/i);
+    if(result === null || result.length!=1)
+        return false;
+    else
+        return true;
+}
+
+// return the color of the text
+function setColor(data){
+    var color = data.split(" ");
+    if(color.length > 1)
+        return color[1];
+    else
+        return 0;
 }
